ce2f414798ddebc7be76204996774d0060006782
semyonm0r <121125939+semyonm0r@users.noreply.github.com>
2024-10-06 12:55:43 +0300

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,88 @@
+# Create the VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags = {
+    Name = "${var.tag_prefix}-vpc"
+    }
+}
+
+# Create the internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.tag_prefix}-igw"
+  }
+}
+
+
+# Create the public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.vpc_public_subnets_cidr_block
+  map_public_ip_on_launch = true
+  availability_zone       = var.aws_azs
+
+  tags = {
+    Name = "${var.tag_prefix}-pubsubnet"
+  }
+
+}
+
+# Create the route table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}-rtable"
+  }
+}
+
+# Assign the public route table to the public subnet
+resource "aws_route_table_association" "public_rt_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Create the security group
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh_http"
+  description = "Allow ssh http inbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}-sg"
+  }
+}
